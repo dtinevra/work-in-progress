f473e3093a104b292778241ef69421654ac11736
Daniel Tinevra <tinevradaniel@gmail.com>
2025-09-27 14:17:56 -0500

    Commit revomal making last commits move

--- js/modules/infrastructure/factory_task.js
+++
@@ -1,9 +0,0 @@
-import { Task } from "../model/task.js";
-
-
-export class TaskFactory {
-    static create(id, description, rational, completed, tags = []) {
-        console.log(`TaskFactory.create() returning: Task(id=${id}})`);
-        return new Task(id, description, rational, completed, tags);
-    }
-}
